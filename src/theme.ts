5c822d08591a6b9006009e166cfc23da1e9ecd28
khangnguyen2100 <khangnguyen23112100@gmail.com>
2023-04-24 23:42:44 +0700

    add theme

--- src/theme.ts
+++ src/theme.ts
@@ -1,30 +1,78 @@
-import { Roboto } from 'next/font/google';
+import { Open_Sans } from 'next/font/google';
 import { createTheme } from '@mui/material/styles';
-import { red } from '@mui/material/colors';
+import type { PaletteMode } from '@mui/material';
 
-export const roboto = Roboto({
-  weight: ['300', '400', '500', '700'],
+export const openSans = Open_Sans({
+  weight: ['300', '400', '500', '600', '700', '800'],
   subsets: ['latin'],
   display: 'swap',
   fallback: ['Helvetica', 'Arial', 'sans-serif'],
 });
+const getDesignTokens = (mode: PaletteMode) => ({
+  palette: {
+    mode,
+    ...(mode === 'light'
+      ? {
+          // palette values for light mode
+          primary: {
+            main: '#ff5414',
+          },
+          secondary: {
+            main: '#0272c4',
+          },
+          divider: '#cccccc',
+          text: {
+            primary: '#222222', // headings
+            secondary: '#7e8183', // text
+            contrastText: '#222222',
+          },
+          background: {
+            default: '#dae0e6',
+            paper: '#ffffff',
+          },
+        }
+      : {
+          // palette values for dark mode
+          primary: {
+            main: '#ff5414',
+          },
+          secondary: {
+            main: '#c8cbcd',
+          },
+          divider: '#343536',
+          text: {
+            primary: '#d7dadc', // headings
+            secondary: '#888a8c', // text
+            contrastText: '#d7dadc',
+          },
+          background: {
+            default: '#030303',
+            paper: '#1a1a1b',
+          },
+        }),
+  },
+});
+const palette = getDesignTokens('light').palette;
 
 // Create a theme instance.
 const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#556cd6',
-    },
-    secondary: {
-      main: '#19857b',
-    },
-    error: {
-      main: red.A400,
-    },
-  },
+  palette,
   typography: {
-    fontFamily: roboto.style.fontFamily,
+    fontFamily: openSans.style.fontFamily,
+    h1: {
+      color: palette.text.contrastText,
+    },
+    h2: {
+      color: palette.text.contrastText,
+    },
+    h3: {
+      color: palette.text.contrastText,
+    },
+    h4: {
+      color: palette.text.contrastText,
+    },
   },
+  spacing: 4,
 });
 
 export default theme;
