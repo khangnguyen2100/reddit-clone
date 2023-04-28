df3ca6019be0ba1c6441779b9600410a48bf91bd
khangnguyen2100 <khangnguyen23112100@gmail.com>
2023-04-28 21:26:26 +0700

    change color

--- src/components/NavBar/RightContent/Icons.tsx
+++ src/components/NavBar/RightContent/Icons.tsx
@@ -1,11 +1,11 @@
 import { Divider, IconButton } from '@mui/material';
 import React from 'react';
 import { BsArrowUpRightCircle, BsChatDots } from 'react-icons/bs';
-import { GrAdd } from 'react-icons/gr';
 import {
+  IoAddSharp,
   IoFilterCircleOutline,
   IoNotificationsOutline,
-  IoVideocamOutline,
+  IoVideocamOutline
 } from 'react-icons/io5';
 type IconBtnProps = {
   onClick?: () => void;
@@ -27,28 +27,28 @@ const Icons = () => {
     <div className='flex-center gap-2 '>
       <div className='flex-center gap-2 mdd:hidden'>
         <IconBtn>
-          <BsArrowUpRightCircle fontSize={20} />
+          <BsArrowUpRightCircle color='text-secondary' fontSize={20} />
         </IconBtn>
         <IconBtn>
-          <IoFilterCircleOutline fontSize={20} />
+          <IoFilterCircleOutline color='text-secondary' fontSize={20} />
         </IconBtn>
         <IconBtn>
-          <IoVideocamOutline fontSize={20} />
+          <IoVideocamOutline color='text-secondary' fontSize={20} />
         </IconBtn>
+        <Divider
+          variant='middle'
+          orientation='vertical'
+          className='h-7 border border-text-secondary'
+        />
       </div>
-      <Divider
-        variant='middle'
-        orientation='vertical'
-        className='h-7 border border-divider'
-      />
       <IconBtn>
-        <BsChatDots fontSize={20} />
+        <BsChatDots color='text-secondary' fontSize={20} />
       </IconBtn>
       <IconBtn>
-        <IoNotificationsOutline fontSize={20} />
+        <IoNotificationsOutline color='text-secondary' fontSize={20} />
       </IconBtn>
       <IconBtn>
-        <GrAdd fontSize={20} />
+        <IoAddSharp color='text-secondary' fontSize={20} />
       </IconBtn>
     </div>
   );
