9213478702f3f49cf02879e974fe61ec3d717937
khangnguyen2100 <khangnguyen23112100@gmail.com>
2023-05-02 20:02:00 +0700

    add link for logo

--- src/components/NavBar/Logo.tsx
+++ src/components/NavBar/Logo.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image';
 import { Box } from '@mui/material';
+import Link from 'next/link';
 
 import redditFace from '/public/images/redditFace.svg';
 import redditText from '/public/images/redditText.svg';
 
 const Logo = () => {
   return (
-    <Box className='flex items-center'>
+    <Box component={Link} href={'/'} className='flex items-center'>
       <Image src={redditFace} alt='logo' width={32} />
       <Image src={redditText} alt='logo' width={57} />
     </Box>
