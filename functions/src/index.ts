d12a568c8ce68233aef04244c1d9ce0415192a89
khangnguyen2100 <khangnguyen23112100@gmail.com>
2023-05-02 11:15:45 +0700

    init and deploy firebase function

---
+++ functions/src/index.ts
@@ -0,0 +1,15 @@
+// // Start writing functions
+// // https://firebase.google.com/docs/functions/typescript
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+
+admin.initializeApp();
+const db = admin.firestore();
+
+export const createUserDocument = functions.auth
+    .user()
+    .onCreate(async (user) => {
+      db.collection("users")
+          .doc(user.uid)
+          .set(JSON.parse(JSON.stringify(user)));
+    });
