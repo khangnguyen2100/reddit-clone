7f3c5bbf8fc617f8aa01cad3fc8fd4accb68d658
khangnguyen2100 <khangnguyen23112100@gmail.com>
2023-05-15 19:09:31 +0700

    remove un need func here

--- src/components/NavBar/Logo.tsx
+++ src/components/NavBar/Logo.tsx
@@ -5,22 +5,9 @@ import Link from 'next/link';
 import redditFace from '/public/images/redditFace.svg';
 import redditText from '/public/images/redditText.svg';
 
-import { useSetRecoilState } from 'recoil';
-
-import { defaultState, directoryMenuState } from 'src/atoms/directoryAtom';
-
 const Logo = () => {
-  const setDirectoryMenuState = useSetRecoilState(directoryMenuState);
-  const handleBackToHomePage = () => {
-    setDirectoryMenuState(defaultState);
-  };
   return (
-    <Box
-      component={Link}
-      href='/'
-      onClick={handleBackToHomePage}
-      className='flex items-center gap-x-2'
-    >
+    <Box component={Link} href='/' className='flex items-center gap-x-2'>
       <Image src={redditFace} alt='logo' width={32} />
       <Image src={redditText} alt='logo' width={57} className='mdd:hidden' />
     </Box>
