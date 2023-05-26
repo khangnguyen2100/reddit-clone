1f7d57222a38829edc4c1133df69745ed823ff95
khangnguyen2100 <khangnguyen23112100@gmail.com>
2023-05-26 11:41:10 +0700

    lint fix

--- src/components/User/ActivityTabs/ActivityTabs.tsx
+++ src/components/User/ActivityTabs/ActivityTabs.tsx
@@ -1,4 +1,4 @@
-import { Box, Tab, Tabs, Typography } from '@mui/material';
+import { Box, Tab, Tabs } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 import React from 'react';
 import SwipeableViews from 'react-swipeable-views';
